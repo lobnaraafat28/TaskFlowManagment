2290e6c33288337eb53b3d02f347a8b115759e70
lobna <lobnaraafat23@gmail.com>
2026-04-09 01:07:22 +0200

    UnitOfWork Implementation

---
+++ TaskFlow.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Core.Interfaces
+{
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+        Task<int> CompleteAsync();
+    }
+}
---
+++ TaskFlow.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Core.Interfaces;
+using TaskFlow.Infrastructure.Context;
+
+namespace TaskFlow.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+            return 0;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            return new GenericRepository<T>(_context);
+        }
+    }
+}
